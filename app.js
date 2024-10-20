faa0ad6a33a4c074681066e73a35fa973f14e8bb
binayakjoshi <binayakj@pm.me>
2024-10-20 18:29:02 +0000

    Update app.js

--- app.js
+++ app.js
@@ -42,5 +42,5 @@ app.use((error, req, res, next) => {
 });
 mongoose
 	.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dfagd0o.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
-	.then(()=>app.listen(5000))
+	.then(()=>app.listen(process.env.PORT))
 	.catch((err)=>console.log(err));
